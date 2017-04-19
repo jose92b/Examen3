3629719194447b63842ef5f9ba6ff7e4a8a47c71
kevmc7 <kevin7_mc@hotmail.com>
2017-04-18 18:49:49 -0600

    1st commit

---
+++ Pulperia/Examen3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Examen3.Startup))]
+namespace Examen3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
