db0a658b86e5ce9a8d24b1faa2002235a18cff4b
kevmc7 <kevin7_mc@hotmail.com>
2017-04-18 19:12:20 -0600

    2nd commit JoseB.

---
+++ Pulperia/Examen3/ControladoraBD.cs
@@ -0,0 +1,46 @@
+﻿using Clase9.BD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Examen3
+{
+    public class ControladoraBD
+    {
+
+        Adaptador adaptador = new Adaptador();
+
+        internal void agregar(Entidad persona)
+        {
+            String consulta = "insert into Reserva(tipo_reserva, fecha_reserva, nombre_cliente, cedula_cliente) values (@tipo_reserva,@fecha,@cliente,@cedula);";
+            SqlCommand comando = new SqlCommand(consulta);
+
+            SqlParameter tipo_reserva = new SqlParameter("@tipo_reserva", SqlDbType.VarChar, 10);
+            SqlParameter fecha_reserva = new SqlParameter("@fecha", SqlDbType.VarChar, 10);
+            SqlParameter nombre = new SqlParameter("@cliente", SqlDbType.VarChar, 30);
+            SqlParameter cedula = new SqlParameter("@cedula", SqlDbType.VarChar, 30);
+
+            comando.Parameters.Add("@tipo_reserva", SqlDbType.VarChar, 10).Value = persona;
+            comando.Parameters.Add("@fecha", SqlDbType.VarChar, 10).Value = persona;
+            comando.Parameters.Add("@cliente", SqlDbType.VarChar, 30).Value = persona;
+            comando.Parameters.Add("@cedula", SqlDbType.VarChar, 30).Value = persona;
+
+            adaptador.insertar(comando);
+        }
+
+        internal void ConsultarProveedor(Entidad persona)
+        {
+            String consulta = "select id, empresa, cont_nombbre, cont_telefono, cont_email, dias_espera from Proveedor where empresa = 'P&G'";
+            adaptador.consultar(consulta);
+        }
+
+        internal void ConsultarProducto(Entidad persona)
+        {
+            String consulta = "select tipo_reserva as [Tipo de reserva], fecha_reserva as Fecha, nombre_cliente as Cliente, cedula_cliente as Cedula from reserva";
+            adaptador.consultar(consulta);
+        }
+    }
+}
---
+++ Pulperia/Examen3/Entidad.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen3
+{
+    public class Entidad
+    {
+        String nom_empresa;
+        String cont_nombre;
+        String cont_telefono;
+        String cont_email;
+        String dias_espera;
+
+
+        public Entidad(Object[] objeto)
+        {
+            this.nom_empresa = objeto[0].ToString();
+            this.cont_nombre = objeto[1].ToString();
+            this.cont_telefono = objeto[2].ToString();
+            this.cont_email = objeto[3].ToString();
+            this.dias_espera = objeto[4].ToString();
+
+
+        }
+        public String Nom_Empresa
+        {
+            get { return nom_empresa; }
+            set { nom_empresa = value; }
+        }
+        public String Cont_Nombre
+        {
+            get { return cont_nombre; }
+            set { cont_nombre = value; }
+        }
+        public String Cont_Telefono
+        {
+            get { return cont_telefono; }
+            set { cont_telefono = value; }
+        }
+        public String Cont_Email
+        {
+            get { return cont_email; }
+            set { cont_email = value; }
+        }
+
+        public String Dias_Espera
+        {
+            get { return dias_espera; }
+            set { dias_espera = value; }
+        }
+
+    }
+}
